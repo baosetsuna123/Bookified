40b122d042235dd6812a08578d55b1105a9a87fb
Bảo Trần <baohse321@gmail.com>
2026-02-28 11:23:47 +0700

    Integrate mongodb

--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -26,6 +26,7 @@ const UploadForm = () => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setIsMounted(true);
   }, []);
 
